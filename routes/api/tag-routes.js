3c9a1b0afade12f84a8e09ce6b56c9e8e4371938
Olivia <osue01@gmail.com>
2024-05-16 18:24:37 -1000

    pt2

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,11 +53,37 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  // update a tag's name by its `id` value
+  try {
+    const tagData = await tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  // delete on tag by its `id` value
+  try {
+    const tagData = await tag.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
